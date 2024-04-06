992f0b9743e77cf8eb12ecce5e03a3697aebbac9
Sarah Wilsoncook <sarahwilsoncook@Sarahs-MBP.fritz.box>
2024-04-06 10:54:44 +0200

    update title

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <h1>Canine Classifier</h1>
+      <h1>Canine Classifier V2</h1>
       <Link to={`results`}>Upload doggo</Link>
     </>
   );
